e921c597b66f48241a7d5b86b9cb2427ed614e95
Alex Tayron <57293582+AlexTayron@users.noreply.github.com>
2022-11-22 21:19:37 -0300

    finalizando primeira parte

--- apps-testaveis-node/src/entities/appointment.ts
+++ apps-testaveis-node/src/entities/appointment.ts
@@ -1,26 +1,26 @@
-interface AppointmentProps{
-   customer: string
-   startsAt: Date
-   endsAt: Date
+interface AppointmentProps {
+  customer: string;
+  startsAt: Date;
+  endsAt: Date;
 }
 
 export class Appointment {
-  private props: AppointmentProps
+  private props: AppointmentProps;
 
-  get customer(){
-    return this.props.customer
+  get customer() {
+    return this.props.customer;
   }
-  get startsAt(){
-    return this.props.startsAt
+  get startsAt() {
+    return this.props.startsAt;
   }
-  get endsAt(){
-    return this.props.endsAt
+  get endsAt() {
+    return this.props.endsAt;
   }
-  constructor(props: AppointmentProps){
-    const { startsAt, endsAt } = props
-    if(endsAt <= startsAt){
-      throw new Error ('Invalid end date')
+  constructor(props: AppointmentProps) {
+    const { startsAt, endsAt } = props;
+    if (endsAt <= startsAt) {
+      throw new Error("Invalid end date");
     }
-    this.props = props
+    this.props = props;
   }
 }
---
+++ apps-testaveis-node/src/use-cases/create-appointment.ts
@@ -0,0 +1,13 @@
+import { Appointment } from "../entities/appointment";
+
+interface CreateAppointmentRequest {
+  customer: string;
+  startsAt: Date;
+  endsAt: Date;
+}
+type CreateAppointmentResponse = Appointment;
+export class CreateAppointment {
+  async execute(
+    request: CreateAppointmentRequest
+  ): Promise<CreateAppointmentResponse> {}
+}
